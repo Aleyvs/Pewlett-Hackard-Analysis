d0e868b117d290914ef5ff35e2dd93cdc5038b18
Alejandra Villarreal <alejandra.y.villarreal21@gmail.com>
2022-06-24 22:11:45 -0500

    added tables from skill drill

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -103,3 +103,109 @@ LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
+
+-- create new table showing Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retirement_dept_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- employee information: A list of employees containing 
+-- their unique employee number, their last name, first name, gender, and salary
+
+--step1. get employee information and crate a temporary table
+
+SELECT emp_no,
+    first_name,
+    last_name,
+    gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Step2. add the columns from salaries that are needed and join
+
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+    --INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+         AND (de.to_date = '9999-01-01');
+
+
+--Management: A list of managers for each department, including
+--the department number, name, and the manager's employee number, last name, first name, 
+--and the starting and ending employment dates
+
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+
+-- Department Retirees: An updated current_emp list that includes everything it currently has,
+--  but also the employee's departments
+
+SELECT  ce.emp_no,
+        ce.first_name,
+        ce.last_name,
+        ce.to_date,
+        d.dept_name
+INTO dept_info
+FROM current_emp as ce
+    INNER JOIN dept_emp as de
+        ON (ce.emp_no = de.emp_no)
+    INNER JOIN departments as d
+        ON (de.dept_no = d.dept_no);
+
+-- Create a query that will return only the information relevant to the Sales team. 
+-- The requested list includes:
+--     Employee numbers
+--     Employee first name
+--     Employee last name
+--     Employee department name
+
+SELECT  di.emp_no,
+        di.first_name,
+        di.last_name,
+        di.dept_name
+INTO sales_info
+FROM dept_info as di
+WHERE di.dept_name = 'Sales';
+
+-- Create another query that will return the following information for the Sales and Development teams:
+--     Employee numbers
+--     Employee first name
+--     Employee last name
+--     Employee department name
+
+
+SELECT  di.emp_no,
+        di.first_name,
+        di.last_name,
+        di.dept_name
+INTO sales_dev_info
+FROM dept_info as di
+WHERE di.dept_name IN ('Sales', 'Development');
